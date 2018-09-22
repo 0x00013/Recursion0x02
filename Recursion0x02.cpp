fb8e89912d6aa8ef65a7fe951710c037c0a6dcd9
0x00013 <32619798+0x00013@users.noreply.github.com>
2018-09-22 11:58:29 -0500

    Add files via upload

---
+++ Recursion0x02.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int Contar(int arre[],int tam,int x,int y); 
+
+int main(){
+    int tam=0;
+    int arre[10];
+    int x=0;
+    int r=0;
+    int y=0;
+
+
+    cout<<"Cuantos numeros son?"<<endl;
+	cin>>tam;
+
+    for(int i=0;i<tam;i++){
+        cout<<"Ingresa los numeros"<<endl;
+        cin>>arre[i];
+    }
+
+    cout<<"Ingrese numero que desea ver repetido"<<endl;
+    cin>>x;
+    
+    
+    r=Contar(arre, tam-1, x, y);
+    cout<<"El numero se repite:"<<" "<<r<<endl;
+
+    return 0;
+}
+
+int Contar(int arre[], int tam, int x, int y){
+    
+    if(tam==-1){
+        return y;
+    }
+
+    if(arre[tam]==x){
+        y++;
+    }   
+    return Contar(arre, tam-1, x, y);
+}
